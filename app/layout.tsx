df8d3141a5dcecd9c2fab1abfb8a972c852512ef
MPhuong <nmphuong57007@gmail.com>
2025-10-08 02:17:14 +0700

    fix: hotfix ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import ChatbotButton from "@/components/chatbot-button";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
+          <SpeedInsights />
           {children}
           <ChatbotButton />
         </ThemeProvider>
