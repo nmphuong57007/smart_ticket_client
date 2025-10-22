ca317c3ffa86a89709ad402c9b719d7a60d6aa49
Thuy Thu <ntthuy188200@gmail.com>
2025-10-23 03:05:15 +0700

    fix

--- app/(main)/combos/page.tsx
+++ app/(main)/combos/page.tsx
@@ -1,5 +1,10 @@
+import React, { Suspense } from "react";
 import ComboContainer from "@/modules/Combos/combo-container";
 
 export default function CombosPage() {
-  return <ComboContainer />;
+  return (
+    <Suspense fallback={<div className="py-12 text-center">Đang tải...</div>}>
+      <ComboContainer />
+    </Suspense>
+  );
 }
