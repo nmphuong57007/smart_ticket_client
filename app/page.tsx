bcd94752c04f6754d0dac5d6352d76cdb479d4c5
ntthuy18 <ntthuy188200@gmail.com>
2025-11-12 19:32:02 +0700

    hotfix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-import ProtectedLayout from "@/components/layouts/protected-layout";
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useLayoutEffect } from "react";
+
+import { redirectConfig } from "@/helpers/redirect-config";
 
 export default function HomePage() {
-  return <ProtectedLayout>HomePage</ProtectedLayout>;
+  const pathname = usePathname();
+  const router = useRouter();
+
+  useLayoutEffect(() => {
+    if (pathname === "/") {
+      router.replace(redirectConfig.home);
+    }
+  }, [pathname, router]);
+
+  return <></>;
 }
